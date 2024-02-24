93d2c6ef32f8998c7c42ab0a1adf6570d58b0cce
Marco Gomes <mgsantos89@gmail.com>
2024-02-24 09:57:17 -0300

    Create NodeGroups

--- fullcycle-terraform/kubernetes-cluster/terraform.tfvars
+++ fullcycle-terraform/kubernetes-cluster/terraform.tfvars
@@ -1,3 +1,6 @@
 prefix = "eks"
 cluster_name = "mgsantos"
 retention_days = 30
+desired_size = 2
+max_size = 4
+min_size = 2
--- fullcycle-terraform/kubernetes-cluster/variables.tf
+++ fullcycle-terraform/kubernetes-cluster/variables.tf
@@ -3,5 +3,7 @@ variable "prefix" {
 variable "cluster_name" {
 }
 variable "retention_days" {
-  
 }
+variable "desired_size"{}
+variable "max_size" {}
+variable "min_size"{}
